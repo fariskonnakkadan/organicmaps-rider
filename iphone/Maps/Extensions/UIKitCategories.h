organicmaps/organicmaps

[ios] Fix contentEdgeInsets/imageEdgeInsets deprecation warnings
These buttons do not use UIButton.Configuration (they rely on the setStyle theme system and, in one case, a gradient sublayer), so a real Configuration migration would mean reworking the theming architecture and risks visual regressions. Instead add a small UIButton (EdgeInsets) category whose setContentEdgeInsetsCompat:/setImageEdgeInsetsCompat: wrap the still-valid pre-iOS 15 setters behind a localized -Wdeprecated-declarations pragma, keeping behavior identical and the warning out of the Swift call sites.

Signed-off-by: Alexander Borsuk <me@alex.bio>

iphone/Maps/Extensions/UIKitCategories.h
@@ -82,3 +82,13 @@ static inline CGFloat LengthCGPoint(CGPoint point)
 + (UIImage *)imageWithColor:(UIColor *)color;
 
 @end
+
+@interface UIButton (EdgeInsets)
+
+/// Wrappers around the pre-iOS 15 contentEdgeInsets / imageEdgeInsets setters.
+/// These buttons don't use UIButton.Configuration, so the properties still apply;
+/// the wrappers keep the iOS 15 deprecation warning out of the Swift call sites.
+- (void)setContentEdgeInsetsCompat:(UIEdgeInsets)insets;
+- (void)setImageEdgeInsetsCompat:(UIEdgeInsets)insets;
+
+@end

iphone/Maps/Extensions/UIKitCategories.m
@@ -295,3 +295,22 @@
 }
 
 @end
+
+@implementation UIButton (EdgeInsets)
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+- (void)setContentEdgeInsetsCompat:(UIEdgeInsets)insets
+{
+  self.contentEdgeInsets = insets;
+}
+
+- (void)setImageEdgeInsetsCompat:(UIEdgeInsets)insets
+{
+  self.imageEdgeInsets = insets;
+}
+
+#pragma clang diagnostic pop
+
+@end

iphone/Maps/UI/BottomMenu/TabBar/BottomTabBarButton.swift
@@ -30,7 +30,7 @@ class BottomTabBarButtonRenderer {
     if let imageInsets = style.imageContainerInsets {
       control.contentVerticalAlignment = .fill
       control.contentHorizontalAlignment = .fill
-      control.imageEdgeInsets = imageInsets
+      control.setImageEdgeInsetsCompat(imageInsets)
     }
     if let backgroundColor = style.backgroundColor {
       control.backgroundColor = backgroundColor

iphone/Maps/UI/Help/AboutController/Views/DonationView.swift
@@ -57,7 +57,7 @@ final class DonationView: UIButton {
     actionButton.titleLabel?.allowsDefaultTighteningForTruncation = true
     actionButton.titleLabel?.adjustsFontSizeToFitWidth = true
     actionButton.titleLabel?.minimumScaleFactor = 0.5
-    actionButton.contentEdgeInsets = Constants.actionButtonTitleInsets
+    actionButton.setContentEdgeInsetsCompat(Constants.actionButtonTitleInsets)
     actionButton.isUserInteractionEnabled = false
     actionButton.setStyle(.crowdfundingButton)
 

iphone/Maps/UI/NavigationDashboard/Views/Navigation/NavigationControlView.swift
@@ -98,10 +98,10 @@ final class NavigationControlView: SolidTouchView {
     button.imageView?.contentMode = .scaleAspectFit
     button.contentHorizontalAlignment = .fill
     button.contentVerticalAlignment = .fill
-    button.contentEdgeInsets = UIEdgeInsets(top: Constants.buttonContentInset,
-                                            left: Constants.buttonContentInset,
-                                            bottom: Constants.buttonContentInset,
-                                            right: Constants.buttonContentInset)
+    button.setContentEdgeInsetsCompat(UIEdgeInsets(top: Constants.buttonContentInset,
+                                                   left: Constants.buttonContentInset,
+                                                   bottom: Constants.buttonContentInset,
+                                                   right: Constants.buttonContentInset))
   }
 
   override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {

iphone/Maps/UI/PlacePage/Components/InfoItemView.swift
@@ -50,7 +50,7 @@ final class InfoItemView: UIView {
 
     iconButton.imageView?.contentMode = .scaleAspectFit
     iconButton.addTarget(self, action: #selector(onIconButtonTap), for: .touchUpInside)
-    iconButton.contentEdgeInsets = Constants.iconButtonEdgeInsets
+    iconButton.setContentEdgeInsetsCompat(Constants.iconButtonEdgeInsets)
     iconButton.isHidden = true
 
     accessoryButton.addTarget(self, action: #selector(onAccessoryButtonTap), for: .touchUpInside)

iphone/Maps/UI/PlacePage/Components/PlacePageOSMContributionViewController.swift
@@ -76,7 +76,7 @@ final class PlacePageOSMContributionViewController: UIViewController {
     }
     button.addTarget(self, action: action, for: .touchUpInside)
     button.setStyle(.flatNormalGrayButtonBig)
-    button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+    button.setContentEdgeInsetsCompat(UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8))
     button.titleLabel?.minimumScaleFactor = 0.5
     button.titleLabel?.adjustsFontSizeToFitWidth = true
     button.titleLabel?.allowsDefaultTighteningForTruncation = true

fariskonnakkadan/organicmaps-rider

Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

iphone/Maps/Extensions/UIKitCategories.h
@@ -82,3 +82,13 @@ static inline CGFloat LengthCGPoint(CGPoint point)
 + (UIImage *)imageWithColor:(UIColor *)color;
 
 @end
+
+@interface UIButton (EdgeInsets)
+
+/// Wrappers around the pre-iOS 15 contentEdgeInsets / imageEdgeInsets setters.
+/// These buttons don't use UIButton.Configuration, so the properties still apply;
+/// the wrappers keep the iOS 15 deprecation warning out of the Swift call sites.
+- (void)setContentEdgeInsets:(UIEdgeInsets)insets;
+- (void)setImageEdgeInsets:(UIEdgeInsets)insets;
+
+@end

iphone/Maps/Extensions/UIKitCategories.m
@@ -295,3 +295,22 @@
 }
 
 @end
+
+@implementation UIButton (EdgeInsets)
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+
+- (void)setContentEdgeInsets:(UIEdgeInsets)insets
+{
+  self.contentEdgeInsets = insets;
+}
+
+- (void)setImageEdgeInsets:(UIEdgeInsets)insets
+{
+  self.imageEdgeInsets = insets;
+}
+
+#pragma clang diagnostic pop
+
+@end

iphone/Maps/UI/BottomMenu/TabBar/BottomTabBarButton.swift
@@ -30,7 +30,7 @@ class BottomTabBarButtonRenderer {
     if let imageInsets = style.imageContainerInsets {
       control.contentVerticalAlignment = .fill
       control.contentHorizontalAlignment = .fill
-      control.imageEdgeInsets = imageInsets
+      control.setImageEdgeInsets(imageInsets)
     }
     if let backgroundColor = style.backgroundColor {
       control.backgroundColor = backgroundColor

iphone/Maps/UI/Help/AboutController/Views/DonationView.swift
@@ -57,7 +57,7 @@ final class DonationView: UIButton {
     actionButton.titleLabel?.allowsDefaultTighteningForTruncation = true
     actionButton.titleLabel?.adjustsFontSizeToFitWidth = true
     actionButton.titleLabel?.minimumScaleFactor = 0.5
-    actionButton.contentEdgeInsets = Constants.actionButtonTitleInsets
+    actionButton.setContentEdgeInsets(Constants.actionButtonTitleInsets)
     actionButton.isUserInteractionEnabled = false
     actionButton.setStyle(.crowdfundingButton)
 

iphone/Maps/UI/NavigationDashboard/Views/Navigation/NavigationControlView.swift
@@ -98,10 +98,10 @@ final class NavigationControlView: SolidTouchView {
     button.imageView?.contentMode = .scaleAspectFit
     button.contentHorizontalAlignment = .fill
     button.contentVerticalAlignment = .fill
-    button.contentEdgeInsets = UIEdgeInsets(top: Constants.buttonContentInset,
-                                            left: Constants.buttonContentInset,
-                                            bottom: Constants.buttonContentInset,
-                                            right: Constants.buttonContentInset)
+    button.setContentEdgeInsets(UIEdgeInsets(top: Constants.buttonContentInset,
+                                             left: Constants.buttonContentInset,
+                                             bottom: Constants.buttonContentInset,
+                                             right: Constants.buttonContentInset))
   }
 
   override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {

iphone/Maps/UI/PlacePage/Components/InfoItemView.swift
@@ -50,7 +50,7 @@ final class InfoItemView: UIView {
 
     iconButton.imageView?.contentMode = .scaleAspectFit
     iconButton.addTarget(self, action: #selector(onIconButtonTap), for: .touchUpInside)
-    iconButton.contentEdgeInsets = Constants.iconButtonEdgeInsets
+    iconButton.setContentEdgeInsets(Constants.iconButtonEdgeInsets)
     iconButton.isHidden = true
 
     accessoryButton.addTarget(self, action: #selector(onAccessoryButtonTap), for: .touchUpInside)

iphone/Maps/UI/PlacePage/Components/PlacePageOSMContributionViewController.swift
@@ -76,7 +76,7 @@ final class PlacePageOSMContributionViewController: UIViewController {
     }
     button.addTarget(self, action: action, for: .touchUpInside)
     button.setStyle(.flatNormalGrayButtonBig)
-    button.contentEdgeInsets = UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+    button.setContentEdgeInsets(UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8))
     button.titleLabel?.minimumScaleFactor = 0.5
     button.titleLabel?.adjustsFontSizeToFitWidth = true
     button.titleLabel?.allowsDefaultTighteningForTruncation = true